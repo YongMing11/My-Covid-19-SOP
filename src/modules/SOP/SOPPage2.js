f80c069ab1eefa0c03bac0c92b16181993a6fb83
He Lin <helinchooi@gmail.com>
2021-08-29 17:40:21 +0800

    Save the weird unstaged files by git add

--- src/modules/SOP/SOPPage2.js
+++ src/modules/SOP/SOPPage2.js
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
 import { StyleSheet, View, SafeAreaView, ScrollView } from "react-native";
 import { Searchbar, List, Menu, Button } from "react-native-paper";
-import { useRoute } from "@react-navigation/native";
 import info from "@mock/sop.json";
 import { Audio } from "expo-av";
-import * as MediaLibrary from "expo-media-library";
-import * as FileSystem from "expo-file-system";
 
 const SOPPage2 = ({ navigation }) => {
   const icon = "chevron-right";
@@ -17,7 +14,7 @@ const SOPPage2 = ({ navigation }) => {
 
   async function playSound(uri) {
     console.log("Loading Sound");
-    console.log(uri)
+    console.log(uri);
     const { sound } = await Audio.Sound.createAsync({ uri });
     // const { sound } = await Audio.Sound.createAsync(require('./5_pengagihan.mp3'));
     setSound(sound);
@@ -45,118 +42,15 @@ const SOPPage2 = ({ navigation }) => {
     });
     setActiveSectors(sectorsResult);
   };
+
   const onPressAction = (sector) => {
     navigation.navigate("SOPPage3", { title: sector });
   };
-  const uploadAudio = async () => {
-    const uri = recording.getURI();
-    // getTranscription(uri);
-    try {
-      const blob = await new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.onload = () => {
-          try {
-            resolve(xhr.response);
-          } catch (error) {
-            console.log("error in uploadAudio when trying to resolve:", error);
-          }
-        };
-        xhr.onerror = (e) => {
-          console.log(e);
-          reject(new TypeError("Network request failed in uploadAudio"));
-        };
-        xhr.responseType = "blob";
-        xhr.open("GET", uri, true);
-        xhr.send(null);
-      });
-      if (blob != null) {
-        // sendAudio(uri);
-        uploadAudioAsync(uri)
-        getTranscription(blob)
-        console.log('fuckelite')
-      } else {
-        console.log("erroor with blob");
-      }
-    } catch (error) {
-      console.log("error in uploadAudio:", error);
-    }
-  };
 
-  // const sendAudio = async (uri) => {
-  //   console.log("start sending audio file");
-  //   const formData = new FormData();
-  //   formData.append('file', {
-  //     uri,
-  //     type: 'audio/flac',
-  //     // could be anything 
-  //     name: 'speech2text'
-  //   });
-  //   // sending blob
-  //   // fetch('https://asia-southeast1-meowmeow-280110.cloudfunctions.net/cloud-source-repositories-test',{
-  //   fetch('http://192.168.0.180:8080/', {
-  //     method: 'POST',
-  //     headers: {
-  //       'content-type': 'multipart/form-data'
-  //       //   // 'Content-Type': 'application/octet-stream'
-  //       //   // 'Content-Type': 'audio/flac'
-  //       // 'Content-Type': 'audio/flac'
-  //     },
-  //     body: formData, // send the audio file
-  //   })
-  //     .then(response => {
-  //       console.log('Get response from Functions.');
-  //       console.log(typeof response);
-  //       // console.log(Object.keys(response));
-  //       // console.log(Object.keys(response).map(key => {
-  //       //   return response[key];
-  //       // }));
-  //       console.log(JSON.stringify(response));
-  //       return response.text();
-  //     })
-  //     .then(res => {
-  //       console.log(res);
-  //     })
-  //     .catch(err => {
-  //       console.log('Error caught in app=>', err);
-  //     }).finally(() => {
-  //       setRecording(undefined);
-  //       console.log('finally');
-  //     });
-
-  //   // send FormData
-  //   // const { uri } = await FileSystem.getInfoAsync(recording.getURI())
-  //   // const formData = new FormData();
-  //   // // formData.append('api_key', CLOUDINARY_API_KEY);
-  //   // formData.append("file", {
-  //   //   // uri: recording.getURI(),
-  //   //   uri: recording.getURI(),
-  //   //   name: recording.getURI().split("/").pop(),
-  //   //   type: "audio/mpeg",
-  //   // });
-  //   // fetch('https://asia-southeast1-meowmeow-280110.cloudfunctions.net/cloud-source-repositories-test',{
-  //   //   method: 'POST',
-  //   //   headers: {
-  //   //     'Content-Type': 'multipart/form-data'
-  //   //   },
-  //   //   body: formData, // send the audio file
-  //   // })
-  //   // .then(response => {
-  //   //   console.log('Get response from Functions.');
-  //   //   return response.text();
-  //   // })
-  //   // .then(res => {
-  //   //   console.log(JSON.stringify(res));
-  //   // })
-  //   // .catch(err => {
-  //   //   console.log('Error caught in app=>',err);
-  //   // }).finally(() => {
-  //   //   setRecording(undefined);
-  //   //   console.log('in finally');
-  //   // });
-  // };
   async function uploadAudioAsync(uri) {
     console.log("Uploading " + uri);
-    let apiUrl = 'http://192.168.1.205:8080';
+    let apiUrl = 'http://192.168.0.180:8080';
+    // let apiUrl = 'https://asia-southeast1-meowmeow-280110.cloudfunctions.net/cloud-source-repositories-test';
     let uriParts = uri.split('.');
     let fileType = uriParts[uriParts.length - 1];
 
@@ -166,7 +60,6 @@ const SOPPage2 = ({ navigation }) => {
       name: `recording.${fileType}`,
       type: `audio/wav`,
     });
-    formData.append('name', "Ng Yong Ming");
 
     let options = {
       method: 'POST',
@@ -181,50 +74,7 @@ const SOPPage2 = ({ navigation }) => {
     return fetch(apiUrl, options);
   }
 
-  const getTranscription = async (uri) => {
-    try {
-      const formData = new FormData();
-      formData.append('file', {
-        uri,
-        type: 'audio/wav',
-        name: 'speech2text.wav'
-      });
-      console.log("DONE FETCHING")
-      // formData.append('user', "NAMENAMENAME");
-      // formData.append('file', uri);
-      const response = fetch('http://192.168.1.205:8080/', {
-        method: 'POST',
-        body: formData
-      }).then((res) => {
-        console.log("TING WEI JING")
-        console.log(JSON.stringify(res))
-      }).catch(() => {
-        console.log("CHOOI HE L:IN")
-      }).finally(() => {
-        console.log("FINALLY")
-      });
-      // const data = await response.json();
-      // console.log(data)
-    } catch (error) {
-      console.log('There was an error', error);
-      // this.stopRecording();
-      // this.resetRecording();
-    }
-    // this.setState({ isFetching: false });
-  }
-
   async function startRecording() {
-    // const formData = new FormData();
-    // formData.append("username", "Groucho");
-    // console.log(formData)
-    // const response = await fetch('http://192.168.0.180:8080/', {
-    //   mode: 'no-cors',
-    //   method: 'POST',
-    //   body: formData
-    // });
-    // const data = await response.json();
-    // console.log(data)
-    // return;
     setIsRecording(true);
     try {
       console.log("Requesting permissions..");
@@ -238,12 +88,12 @@ const SOPPage2 = ({ navigation }) => {
         {
           isMeteringEnabled: true,
           android: {
-            extension: '.wav',
-            // outputFormat: Audio.RECORDING_OPTION_ANDROID_OUTPUT_FORMAT_MPEG_4,
-            // audioEncoder: Audio.RECORDING_OPTION_ANDROID_AUDIO_ENCODER_AAC,
-            sampleRate: 44100,
-            numberOfChannels: 2,
-            bitRate: 128000,
+            extension: '.amr',
+            outputFormat: Audio.RECORDING_OPTION_ANDROID_OUTPUT_FORMAT_AMR_NB,
+            audioEncoder: Audio.RECORDING_OPTION_ANDROID_AUDIO_ENCODER_AMR_NB,
+            sampleRate: 8000,
+            numberOfChannels: 1,
+            bitRate: 12200,
           },
           ios: {
             extension: ".caf",
@@ -256,11 +106,8 @@ const SOPPage2 = ({ navigation }) => {
             linearPCMIsFloat: false,
           },
         }
-        // above structure is get from node_modules
-        //  Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
       );
       setRecording(recording);
-      // console.log(recording);
       console.log("Recording started");
     } catch (err) {
       console.error("Failed to start recording", err);
@@ -271,12 +118,23 @@ const SOPPage2 = ({ navigation }) => {
     console.log("Stopping recording..");
     setIsRecording(false);
 
-    await recording.stopAndUnloadAsync();
+    await recording.stopAndUnloadAsync().catch(err => console.log);
     const uri = recording.getURI();
     console.log('Recording stopped and stored at', uri);
     playSound(uri);
     // sendAudio();
-    uploadAudio();
+    uploadAudioAsync(uri)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res);
+      // sample response if returned text/html
+      // {"type":"default","status":200,"ok":true,"statusText":"","headers":{"map":{"x-cloud-trace-context":"df30dd1559853ecbbe1ff713a4df5533;o=1","content-type":"text/html; charset=utf-8","function-execution-id":"m7gm271eujuc","server":"Google Frontend","date":"Sun, 29 Aug 2021 09:22:14 GMT","alt-svc":"h3=\":443\"; ma=2592000,h3-29=\":443\"; ma=2592000,h3-T051=\":443\"; ma=2592000,h3-Q050=\":443\"; ma=2592000,h3-Q046=\":443\"; ma=2592000,h3-Q043=\":443\"; ma=2592000,quic=\":443\"; ma=2592000; v=\"46,43\"","content-length":"83"}},"url":"https://asia-southeast1-meowmeow-280110.cloudfunctions.net/cloud-source-repositories-test","bodyUsed":false,"_bodyInit":{"_data":{"size":83,"offset":0,"blobId":"9c3368e7-7180-4c69-8215-1ed5949db4e8","__collector":{}}},"_bodyBlob":{"_data":{"size":83,"offset":0,"blobId":"9c3368e7-7180-4c69-8215-1ed5949db4e8","__collector":{}}}}
+      // sample response if returned application/json
+      // {"type":"default","status":200,"ok":true,"statusText":"","headers":{"map":{"content-length":"86","content-type":"application/json","date":"Sun, 29 Aug 2021 09:32:54 GMT","server":"Werkzeug/2.0.1 Python/3.9.1"}},"url":"http://192.168.0.180:8080/","bodyUsed":false,"_bodyInit":{"_data":{"size":86,"offset":0,"blobId":"b89b3973-e195-46ea-9f9f-53e538e19f88","__collector":{}}},"_bodyBlob":{"_data":{"size":86,"offset":0,"blobId":"b89b3973-e195-46ea-9f9f-53e538e19f88","__collector":{}}}}
+    }).catch(err => {
+      console.log(err);
+      return err;
+    });
   }
 
   return (
