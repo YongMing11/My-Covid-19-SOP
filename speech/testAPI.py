ce501bfb7ceb0453e898a27cb49feccda462a361
He Lin <helinchooi@gmail.com>
2021-08-29 13:53:04 +0800

    Able to get text from brother.amr in testAPI.py

--- speech/testAPI.py
+++ speech/testAPI.py
@@ -9,15 +9,18 @@ client = speech.SpeechClient()
 # The name of the audio file to transcribe
 # gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
 # gcs_uri = "gs://ozone-audio-17263/work.m4a"     # default file type by RN
-gcs_uri = "gs://ozone-audio-17263/work.wav"       # produced by Audacity
+# gcs_uri = "gs://ozone-audio-17263/haha12200.m4a"     # another file by RN - by YM
+# gcs_uri = "gs://ozone-audio-17263/work.wav"       # produced by Audacity - working with language_code="en-US", audio_channel_count=2 ONLY
+# gcs_uri = "gs://ozone-audio-17263/haha12200.flac"       # produced by RN-EXPO
+gcs_uri = "gs://ozone-audio-17263/brother.amr"       # produced by RN-EXPO
 
 audio = speech.RecognitionAudio(uri=gcs_uri)
 
 config = speech.RecognitionConfig(
-    # encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-    # sample_rate_hertz=44100,
-    language_code="en-US",
-    audio_channel_count=2,
+    encoding=speech.RecognitionConfig.AudioEncoding.AMR,
+        sample_rate_hertz=8000,
+        language_code="en-US",
+        audio_channel_count=1,
 )
 
 # Detects speech in the audio file
