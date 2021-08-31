1eaf67f3dbcf24e5600328bcd61c49d423de93ce
TINGWEIJING <tingweijingting2000@gmail.com>
2021-08-31 16:10:02 +0800

    Display SOP data in SOP Info Page

--- src/modules/SOP/SOPPage1.js
+++ src/modules/SOP/SOPPage1.js
@@ -1,95 +1,93 @@
 import React, { Component } from "react";
 import { Text, StyleSheet, View, SafeAreaView, ScrollView } from "react-native";
 import { withTheme, Button, Card, Title, Paragraph, Surface } from "react-native-paper";
-import info from '@mock/sop.json';
+import sop_index from "@mock/sop_index.json";
 
 class SOPPage1 extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      phases: info.phases,
-    };
-  }
-  render() {
-    const { colors } = this.props.theme;
-    const { phases } = this.state;
-    const { navigation, route } = this.props;
-    console.log(route.name);
-
-    const onPressAction = (title) => {
-      navigation.navigate('SOPPage2', { title:title })
+    constructor(props) {
+        super(props);
+        this.state = {
+            phases: sop_index["phases"],
+        };
     }
 
-    return (
-      <SafeAreaView style={styles.container}>
-        <ScrollView>
-          <View style={styles.view}>
-            {phases.map((phaseCard, index) => (
-              <Card style={styles.card} key={index} onPress={()=>onPressAction(phaseCard.title)}>
-                <View>
-                  <Text style={styles.cardTitle}>{phaseCard.title}</Text>
-                </View>
-                <Card.Content>
-                  <Paragraph style={styles.textCenter}>
-                    National Recovery Plan
-                  </Paragraph>
-                  <View style={styles.states}>
-                    {
-                    phaseCard.areas.length === 0? 
-                    <Text style={styles.notAvailableMsg}>{'No state or region under this phase'}</Text>:  
-                    phaseCard.areas.map((state) => (
-                      <Text key={state} style={styles.state}>
-                        {`\u2022 ${state}`}
-                      </Text>
-                    ))
-                    }
-                  </View>
-                </Card.Content>
-              </Card>
-            ))}
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    );
-  }
+    render() {
+        const { colors } = this.props.theme;
+        const { phases } = this.state;
+        const { navigation, route } = this.props;
+
+        const onPressAction = (title, index) => {
+            navigation.navigate("SOPPage2", { title: title, index: index });
+        };
+
+        return (
+            <SafeAreaView style={styles.container}>
+                <ScrollView>
+                    <View style={styles.view}>
+                        {phases.map((phaseCard, index) => (
+                            <Card style={styles.card} key={index} onPress={() => onPressAction(phaseCard.title, index)}>
+                                <View>
+                                    <Text style={styles.cardTitle}>{phaseCard.title}</Text>
+                                </View>
+                                <Card.Content>
+                                    <Paragraph style={styles.textCenter}>{phaseCard.subtitle}</Paragraph>
+                                    <View style={styles.states}>
+                                        {phaseCard.areas.length === 0 ? (
+                                            <Text style={styles.notAvailableMsg}>{"No state or region under this phase"}</Text>
+                                        ) : (
+                                            phaseCard.areas.map((state) => (
+                                                <Text key={state} style={styles.state}>
+                                                    {`\u2022 ${state}`}
+                                                </Text>
+                                            ))
+                                        )}
+                                    </View>
+                                </Card.Content>
+                            </Card>
+                        ))}
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+        );
+    }
 }
 export default withTheme(SOPPage1);
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-  },
-  card: {
-    flexDirection: "row",
-    width: "96%",
-    marginBottom: 10,
-    elevation: 6,
-  },
-  textCenter: {
-    textAlign: "center",
-  },
-  cardTitle: {
-    textAlign: "center",
-    fontSize: 25,
-    fontWeight: "bold",
-  },
-  view: {
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 15,
-  },
-  states: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-  },
-  state: {
-    width: "50%",
-    fontSize: 20,
-  },
-  notAvailableMsg:{
-    fontSize: 16,
-    textAlign: "center",
-  }
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+    },
+    card: {
+        flexDirection: "row",
+        width: "96%",
+        marginBottom: 10,
+        elevation: 6,
+    },
+    textCenter: {
+        textAlign: "center",
+    },
+    cardTitle: {
+        textAlign: "center",
+        fontSize: 25,
+        fontWeight: "bold",
+    },
+    view: {
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 15,
+    },
+    states: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+    },
+    state: {
+        width: "50%",
+        fontSize: 20,
+    },
+    notAvailableMsg: {
+        fontSize: 16,
+        textAlign: "center",
+    },
 });
--- src/modules/SOP/SOPPage3.js
+++ src/modules/SOP/SOPPage3.js
@@ -1,111 +1,126 @@
 import React, { Fragment, useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  ScrollView,
-} from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
 import { Searchbar, List } from "react-native-paper";
-import info from '@mock/sop.json';
+import sop_details from "@mock/sop_details.json";
 
-const SOPPage3 = () => {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [activeAccordians, setActiveAccordians] = useState(info.activities);
+const SOPPage3 = ({ route, navigation }) => {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [activeAccordions, setActiveAccordions] = useState(sop_details[route.params.title]);
+    // console.log(sop_details[route.params.title]);
 
-  const onChangeSearch = (query) => {
-    setSearchQuery(query);
-    if(query === ''){setActiveAccordians(info.activities); return;}
-    const result = info.activities.filter(s => {
-      let isIncluded = s.title.toLowerCase().includes(query.toLowerCase());
-      isIncluded = isIncluded || s.content.filter(c => {
-        return c.toLowerCase().includes(query.toLowerCase());
-      }).length !==0;
-      return isIncluded;
-    });
-    console.log(result);
-    setActiveAccordians(result);
-  };
+    const onChangeSearch = (query) => {
+        // setSearchQuery(query);
+        // if (query === "") {
+        //     setActiveAccordions(info.activities);
+        //     return;
+        // }
+        // const result = info.activities.filter((s) => {
+        //     let isIncluded = s.title.toLowerCase().includes(query.toLowerCase());
+        //     isIncluded =
+        //         isIncluded ||
+        //         s.content.filter((c) => {
+        //             return c.toLowerCase().includes(query.toLowerCase());
+        //         }).length !== 0;
+        //     return isIncluded;
+        // });
+        // console.log(result);
+        // setActiveAccordions(result);
+    };
 
-  return (
-    <SafeAreaView style={styles.container}>
-      {/* <ScrollView> */}
-      <View>
-        <Searchbar
-          placeholder="Search in page"
-          onChangeText={onChangeSearch}
-          value={searchQuery}
-          style={styles.searchBar}
-        />
-      </View>
-      <List.Section>
-        {activeAccordians.map((a, index) => (
-          <List.Accordion
-            title={a.title}
-            titleStyle={styles.accordianTitle}
-            style={styles.header}
-            key={index}
-          >
-            <View style={styles.activity}>
-              {a.content.map((c, index) => {
-                return <View style={styles.bulletItem} key={index}>
-                  <Text style={styles.content}>{`\u2022  `}</Text>
-                  <Text style={styles.content}>{c}</Text>
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView>
+                <View>
+                    <Searchbar placeholder="Search in page" onChangeText={onChangeSearch} value={searchQuery} style={styles.searchBar} />
                 </View>
-                })
-              }
-            </View>
-          </List.Accordion>
-        ))}
-      </List.Section>
-      {/* </ScrollView> */}
-    </SafeAreaView>
-  );
+                <List.Section>
+                    {activeAccordions.map((item, index) => (
+                        <List.Accordion title={item.title} titleStyle={styles.accordianTitle} style={styles.header} key={index}>
+                            <View style={styles.activity}>
+                                {item.content.map((detail_item, index) => {
+                                    if (typeof detail_item === "string") {
+                                        return (
+                                            <View style={styles.bulletItem} key={index}>
+                                                <Text style={styles.content}>{`\u2022  `}</Text>
+                                                <Text style={styles.content}>{detail_item}</Text>
+                                            </View>
+                                        );
+                                    } else {
+                                        return (
+                                            <View key={index}>
+                                                <Text style={styles.subheader}>{detail_item.title}</Text>
+                                                {detail_item.content.map((nested_detail_item, nested_index) => {
+                                                    return (
+                                                        <View style={styles.bulletItem} key={nested_index}>
+                                                            <Text style={styles.content}>{`\u2022  `}</Text>
+                                                            <Text style={styles.content}>{nested_detail_item}</Text>
+                                                        </View>
+                                                    );
+                                                })}
+                                            </View>
+                                        );
+                                    }
+                                })}
+                            </View>
+                        </List.Accordion>
+                    ))}
+                </List.Section>
+            </ScrollView>
+        </SafeAreaView>
+    );
 };
 
 export default SOPPage3;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: "#fff",
-  },
-  searchBar: {
-    marginHorizontal: 15,
-    marginTop: 10,
-    marginBottom: 10,
-  },
-  header: {
-    backgroundColor: "#fff",
-    padding: 10,
-    marginHorizontal: 15,
-    marginVertical: 5,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
+    container: {
+        flex: 1,
+        // backgroundColor: "#fff",
     },
-    shadowOpacity: 0.23,
-    shadowRadius: 2.62,
-    elevation: 4,
-  },
-  content: {
-    fontSize: 14,
-  },
-  activity: {
-    marginHorizontal: 15,
-    marginBottom: 15,
-    backgroundColor: "#fff",
-    padding: 10,
-  },
-  bulletItem: {
-    flexDirection: "row",
-    paddingVertical: 5,
-    paddingHorizontal: 10
-  },
-  accordianTitle: {
-    fontSize: 18,
-    color: "black",
-    fontWeight: "bold",
-  },
+    searchBar: {
+        marginHorizontal: 15,
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    header: {
+        backgroundColor: "#fff",
+        padding: 10,
+        marginHorizontal: 15,
+        marginVertical: 5,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+        elevation: 4,
+    },
+    subheader: {
+        fontSize: 15,
+        textAlign: "justify",
+        fontWeight: "bold",
+        paddingTop: 3,
+        paddingBottom: 3,
+    },
+    content: {
+        fontSize: 14,
+        textAlign: "justify",
+    },
+    activity: {
+        marginHorizontal: 15,
+        marginBottom: 15,
+        backgroundColor: "#fff",
+        padding: 10,
+    },
+    bulletItem: {
+        flexDirection: "row",
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+    },
+    accordianTitle: {
+        fontSize: 18,
+        color: "black",
+        fontWeight: "bold",
+    },
 });
